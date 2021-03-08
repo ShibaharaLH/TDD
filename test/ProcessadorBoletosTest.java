5ac6bc315c2c58bc424e41e25dac2fdeb8de2288
Leandro Shibahara <leandrohideyuki@hotmail.com>
2021-03-08 18:49:51 -0300

    Criando os metodos de testes

--- test/ProcessadorBoletosTest.java
+++ test/ProcessadorBoletosTest.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  *
@@ -7,16 +11,40 @@ import static org.junit.Assert.*;
  */
 public class ProcessadorBoletosTest {
     
-    public ProcessadorBoletosTest() {
+    private ProcessadorBoletos processadorBoletos;
+    
+    @BeforeAll
+    public static void inicializa() {
+        processadorBoletos = new ProcessadorBoletos();
     }
     
     @Test
-    public void verificaPagamento() throws Exception {
-        int boletoA = 500.0;
-        int boletoB = 400.0;
-        int boletoC = 600.0;
-        int fatura = 0;
+    public void verificaFaturaPaga() {
+        List<Boleto> boletos = new ArrayList<Boletos>();
+        Boleto boletoA = new Boleto("12345", "08-03-21", 300);
+        Boleto boletoB = new Boleto("54321", "08-03-21", 300);
+        Boleto boletoC = new Boleto("12367", "08-03-21", 300);
+        boletos.add(boletoA);
+        boletos.add(boletoB);
+        boletos.add(boletoC);
+        Pagamento pagamento = new Pagamento("08-03-21", 900, "Leandro Shibahara");
         
-        assertEquals(1500.0, fatura);
+        boolean pagamentoEfetuado = processadorBoletos.validaPagamentoFatura(boletos, pagamento);
+        assertEquals(true, pagamentoEfetuado);
+    }
+    
+    @Test
+    public void verificaFaturaNaoPaga() {
+        List<Boleto> boletos = new ArrayList<Boletos>();
+        Boleto boletoA = new Boleto("12345", "08-03-21", 300);
+        Boleto boletoB = new Boleto("54321", "08-03-21", 300);
+        Boleto boletoC = new Boleto("12367", "08-03-21", 300);
+        boletos.add(boletoA);
+        boletos.add(boletoB);
+        boletos.add(boletoC);
+        Pagamento pagamento = new Pagamento("08-03-21", 1000, "Leandro Shibahara");
+        
+        boolean pagamentoEfetuado = processadorBoletos.validaPagamentoFatura(boletos, pagamento);
+        assertEquals(false, pagamentoEfetuado);
     }
 }
